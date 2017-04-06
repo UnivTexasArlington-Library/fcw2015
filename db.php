4651c71472e48f20ddc98373de5b66db668e9f9e
Krystal Schenk <kschenk@uta.edu>
2017-04-06 11:37:24 -0500

    remove from git

--- db.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db = new PDO('mysql:host=libwebmysqltest.ardclb.uta.edu;dbname=fcw;charset=utf8', 'fcwUser', 'edw00d');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-?>
